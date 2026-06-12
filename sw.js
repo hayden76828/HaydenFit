c7fa8c944150436b3c925e869f9689be39cf90f3
hayden76828 <gw6dwp8m4c@privaterelay.appleid.com>
2026-06-12 16:39:47 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,8 +1,25 @@
-const CACHE = 'haydenfit-v1';
-const ASSETS = ['/', '/index.html', '/manifest.json'];
+const CACHE = 'haydenfit-v2';
+const BASE = self.location.pathname.replace('/sw.js', '');
+const ASSETS = [
+  BASE + '/',
+  BASE + '/index.html',
+  BASE + '/manifest.json'
+];
 
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS).catch(() => {}))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(k => k !== CACHE).map(k => caches.delete(k))
+    ))
+  );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', e => {
